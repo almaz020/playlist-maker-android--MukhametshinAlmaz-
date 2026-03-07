e6bcd96a48a1858e20c4d01edf2cacdaba03f555
almaz020 <AlmazM22@yandex.ru>
2026-03-07 21:35:54 +0300

    feat: added track details screen

---
+++ app/src/main/java/com/almaz/playlistmaker/ui/TrackDetailsScreen.kt
@@ -0,0 +1,126 @@
+package com.almaz.playlistmaker.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+import com.almaz.playlistmaker.R
+import com.almaz.playlistmaker.data.network.Track
+
+@Composable
+fun TrackDetailsScreen(
+    onBack: () -> Unit = {},
+    track: Track,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        PanelHeader(
+            title = "",
+            isButtonEnabled = true,
+            onBack = onBack,
+        )
+        Image(
+            modifier = Modifier.fillMaxWidth().height(312.dp).padding(end = 24.dp, start = 24.dp, top = 26.dp),
+            painter = painterResource(R.drawable.ic_music),
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier.padding(end = 24.dp, start = 24.dp, top = 24.dp),
+            text = track.trackName,
+            fontSize = 22.sp,
+            fontFamily = FontFamily(Font(R.font.yandexsanstextregular)),
+            textAlign = TextAlign.Start,
+        )
+        Text(
+            modifier = Modifier.padding(end = 24.dp, start = 24.dp, top = 12.dp),
+            text = track.artistName,
+            fontSize = 14.sp,
+            fontFamily = FontFamily(Font(R.font.yandexsanstextregular)),
+            textAlign = TextAlign.Start,
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(end = 24.dp, start = 24.dp, top = 54.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            FloatingActionButton(
+                modifier = Modifier,
+                containerColor = Color.Transparent,
+                contentColor = Color.Unspecified,
+                shape = CircleShape,
+                onClick =  {   },
+                elevation = FloatingActionButtonDefaults.elevation(0.dp)
+            )
+            {
+                Icon(
+                    painter = painterResource(R.drawable.new_playlist),
+                    contentDescription = null,
+                )
+            }
+            FloatingActionButton(
+                containerColor = Color.Transparent,
+                contentColor = Color.Unspecified,
+                shape = CircleShape,
+                modifier = Modifier,
+                onClick =  {   },
+                elevation = FloatingActionButtonDefaults.elevation(0.dp)
+            )
+            {
+                Icon(
+                    painter = painterResource(R.drawable.add_favorite),
+                    contentDescription = null,
+                )
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(top = 24.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                modifier = Modifier.padding(start = 16.dp, top = 9.dp, bottom = 8.dp),
+                text = stringResource(R.string.trackTime),
+                fontSize = 13.sp,
+                fontFamily = FontFamily(Font(R.font.yandexsanstextregular)),
+                color = colorResource(R.color.light_gray_for_search_field)
+            )
+            Text(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp, end = 16.dp),
+                text = track.trackTime,
+                fontSize = 13.sp,
+                fontFamily = FontFamily(Font(R.font.yandexsanstextregular)),
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun TrackDetailsScreenPreview() {
+    TrackDetailsScreen({}, Track(1, "Yesterday (Remastered 2009)", "The Beatles", "3:53", "asdasd", true, 12))
+}
